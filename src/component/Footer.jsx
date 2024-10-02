8fc8bf0ba694a7ea779770360c1e5c1cb66d07d0
stay-ha <PRO.git>
2024-10-02 16:12:49 +0900

    시간 안 넘어가는 오류 수정

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,22 +1,44 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { tabContext } from '../App';
 
 function Footer() {
 
   const {currentTab,menuArr,selectMenuhandler} = useContext(tabContext)
-  
-  const now = new Date();
-  const utcNow = now.getTime() + (now.getTimezoneOffset() * 60 * 1000);
   const offset = 9 * 60 * 60 * 1000;
-  const curTimeInKorea = new Date(utcNow + offset);
   
-  const year = curTimeInKorea.getFullYear();
-  const month = curTimeInKorea.getMonth() + 1;
-  const date = curTimeInKorea.getDate();
-  const hour = curTimeInKorea.getHours();
-  const min = curTimeInKorea.getMinutes();
-
-
+  const nowTime = () =>{
+    
+    const now = new Date();
+    const utcNow = now.getTime() + (now.getTimezoneOffset() * 60 * 1000);
+    const curTimeInKorea = new Date(utcNow + offset);
+    
+    let hour = curTimeInKorea.getHours();
+    let min = curTimeInKorea.getMinutes();
+    
+    return `${hour < 13 ? `오전 ${(hour)}` : `오후 ${(hour-12)<10?`${(hour-12)}`: (hour-12)}`}:${min < 10 ? `0${(min)}` : (min)}`
+  }
+  
+  const nowDate = () =>{
+    
+    const now = new Date();
+    const utcNow = now.getTime() + (now.getTimezoneOffset() * 60 * 1000);
+    const curTimeInKorea = new Date(utcNow + offset);
+    
+    let year = curTimeInKorea.getFullYear();
+    let month = curTimeInKorea.getMonth() + 1;
+    let date = curTimeInKorea.getDate();
+    
+    return `${year}-${month < 10 ?  `0${(month)}` : (month)}-${date < 10 ?  `0${(date)}` : (date)}`
+  }
+  
+  const [clock , setClock] = useState(nowTime);
+  const [calender , setCalender] = useState(nowTime);
+  
+  setInterval(()=> {
+    setClock(nowTime);
+    setCalender(nowDate);
+  }, 1000);
+  
   return (
     <footer>
       
@@ -41,9 +63,9 @@ function Footer() {
           </div>
           
           <div className='date'>
-            <span>{hour < 13 ? `오전 ${(hour)}` : `오후 ${(hour-12)<10?`${(hour-12)}`: (hour-12)}`}:{min < 10 ? `0${(min)}` : (min)}</span>
-            <span>{year}-{month < 10 ?  `0${(month)}` : (month)}-{date < 10 ?  `0${(date)}` : (date)}</span>
-          
+            <span>{clock}</span>
+            <span>{calender}</span>
+            
           </div>
           
     </footer>
