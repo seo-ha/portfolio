fc280300bd1c37a76f3e47601708336567068f58
seo-ha <mk096@naver.com>
2025-01-25 17:25:17 +0900

    경력기술서 속성 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,16 +5,16 @@ function Home() {
   const [search, setSearch] = useState("");
   const list = [
    { text : '이력서' , url : process.env.PUBLIC_URL + '/assets/images/김서하_이력서.pdf'}, 
-   { text : '경력기술서' , url : process.env.PUBLIC_URL + '/assets/images/경력기술서.pdf'}, 
+   { text : '경력기술서' , url : process.env.PUBLIC_URL + '/assets/images/경력기술서.docx'}, 
    { text : 'Github' , url : 'https://github.com/seo-ha'}, 
    { text : '#프론트엔드'}, 
    { text : '#퍼블리셔'}, 
-   { text : '#정직한'}, 
-   { text : '#늘 배우는'}, 
+   { text : '#정직한'},   
+   { text : '#늘 배우는'},
    { text : '#성실한'}, 
    { text : '#손이 빠른'},
    { text : '#꼼꼼한'},
-   { text : '#책임감이 있는'},
+   { text : '#책임감이 있는'}, 
   ];
 
   const filterTitle = list.filter((item) => {
